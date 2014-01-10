e14af93e48d2c18e9cd325f63ed616c95c781f56
Rodolfo Dias <rmdias29@gmail.com>
2014-01-10 21:44:49 -0200

    add files

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+
+// package.json
+var pkg = require('./package.json');
+
+//plugins
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+
+console.log(uglify());
+// tasks
+
+// Concat & Minify JS
+gulp.task('minify', function(){
+  var scriptFiles = './assets/js/**/*.js';
+  var scriptDist = './js';
+  
+  gulp.src(scriptFiles)
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest(scriptDist))
+      .pipe(rename('all.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(scriptDist));
+});
+
+// { sass }
+gulp.task('sass', function() {
+  var sassFiles = './assets/sass/**/*.sass';
+  var cssDist = './css/main.css';
+
+  gulp.src(sassFiles)
+      .pipe(concat('all.sass'))
+      .pipe(sass({unixNewlines: true, style: 'compressed'}))
+      .pipe(gulp.dest('./css'));
+});
+
+
+// The default task (called when you run `gulp`)
+gulp.task('default', function() {
+  gulp.run('minify', 'sass');
+
+  // Watch files and run tasks if they change
+  gulp.watch('./assets/**/*', function() {
+    gulp.run('minify');
+    gulp.run('sass');
+    
+    console.log('------------- END -------------');
+
+  });
+});
